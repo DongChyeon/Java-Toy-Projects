1ba13254f4c3e1ff139a60eb100e615d3a0329b8
DongChyeon <kimdonghyun90766@gmail.com>
2020-08-11 23:00:17 +0900

    Add calculation function

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,6 +13,8 @@ import javax.swing.JTextField;
 public class Calculator extends JFrame {
 
 	private JTextField inputSpace;
+	private String num = "";
+	private ArrayList<String> equation = new ArrayList<String>();
 	
 	public Calculator() {
 		setLayout(null);
@@ -25,7 +30,7 @@ public class Calculator extends JFrame {
 		buttonPanel.setLayout(new GridLayout(4, 4, 10, 10));
 		buttonPanel.setBounds(8, 90, 270, 235);
 		
-		String button_names[] = { "C", "¡À", "¡¿", "=", "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "0" };
+		String button_names[] = { "C", "Ã·", "Ã—", "=", "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "0" };
 		JButton buttons[] = new JButton[button_names.length];
 		
 		for (int i = 0; i < button_names.length; i++) {
@@ -36,13 +41,14 @@ public class Calculator extends JFrame {
 			else buttons[i].setBackground(Color.GRAY);
 			buttons[i].setForeground(Color.WHITE);
 			buttons[i].setBorderPainted(false);
+			buttons[i].addActionListener(new PadActionListener());
 			buttonPanel.add(buttons[i]);
 		}
 		
 		add(inputSpace);
 		add(buttonPanel);
 		
-		setTitle("°è»ê±â");
+		setTitle("ê³„ì‚°ê¸°");
 		setVisible(true);
 		setSize(300, 370);
 		setLocationRelativeTo(null);
@@ -50,8 +56,76 @@ public class Calculator extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
+	class PadActionListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			String operation = e.getActionCommand();
+			if (operation.equals("C")) {
+				inputSpace.setText("");
+			} else if (operation.equals("=")) {
+				String result = Double.toString(calculate(inputSpace.getText()));
+				inputSpace.setText("" + result);
+				num = "";
+			} else {
+				inputSpace.setText(inputSpace.getText() + e.getActionCommand());
+			}
+		}
+	}
+	
+	private void fullTextParsing(String inputText) {
+		equation.clear();
+		
+		for (int i = 0; i < inputText.length(); i++) {
+			char ch = inputText.charAt(i);
+			
+			if (ch == '-' | ch == '+' | ch == 'Ã—' | ch == 'Ã·') {
+				equation.add(num);
+				num = "";
+				equation.add(ch + "");
+			} else {
+				num = num + ch;
+			}
+		}
+		equation.add(num);
+	}
+	
+	public double calculate(String inputText) {
+		fullTextParsing(inputText);
+		
+		double prev = 0;
+		double current = 0;
+		String mode = "";
+		
+		for (String s : equation) {
+			if (s.equals("+")) {
+				mode = "add";
+			} else if (s.equals("-")) {
+				mode = "sub";
+			} else if (s.equals("Ã—")) {
+				mode = "mul";
+			} else if (s.equals("Ã·")) {
+				mode = "div";
+			} else {
+				current = Double.parseDouble(s);
+				if (mode == "add" ) {
+					prev += current;
+				} else if (mode == "sub") {
+					prev -= current;
+				} else if (mode == "mul") {
+					prev *= current;
+				} else if (mode == "div") {
+					prev /= current;
+				} else {
+					prev = current;
+				}
+			}
+		}
+		
+		return prev;
+	}
+	
 	public static void main(String[] args) {
 		new Calculator();
 	}
 
 }
+
